069c4997867e1250ba679b2fc443d2d04d95a56b
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-08-09 15:15:01 +0200

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'tasks/show'
+
+  # get 'tasks/new'
+
+  # get 'tasks/create'
+
+  # get 'tasks/edit'
+
+  # get 'tasks/update'
+
+  # get 'tasks/destroy'
+
+  # get 'tasks/index'
+  get root to: "tasks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
 end
